b6907aa8131c3029a66e01a34bb728faf5ae7e2f
Johan Stokking <johan@stokking.nl>
2018-03-09 11:43:46 -0600

    Fix formatting

--- webapp/server.js
+++ webapp/server.js
@@ -124,9 +124,9 @@ function exitHandler(options) {
     if (options.exit) process.exit();
 }
 
-process.on('exit', exitHandler.bind(null, { cleanup:true }));
-process.on('SIGINT', exitHandler.bind(null, { exit:true }));
-process.on('SIGUSR1', exitHandler.bind(null, { exit:true }));
-process.on('SIGUSR2', exitHandler.bind(null, { exit:true }));
-process.on('uncaughtException', exitHandler.bind(null, { exit:true }));
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
 
